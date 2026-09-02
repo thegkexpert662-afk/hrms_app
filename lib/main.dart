562b6468fab78f8eb4d9e2550937ea6c9e147a95
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-02 11:28:00 +0530

    Start HRMS application modules

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
+import 'app/hrms_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const HrmsApp());
-}
-
-class HrmsApp extends StatelessWidget {
-  const HrmsApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HRMS',
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'ai pagal hai',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(HrmsApp(service: HrmsService()));
 }
